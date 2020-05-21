f2386efa330335ca8873c71886cbe06a2d4cdd84
Juan Camilo Cordoba <jcordoba87@uan.edu.co>
2020-05-20 19:54:18 -0500

    Prueba JUnit

---
+++ AdministracionInventarios/src/PruebasJUnit/SesionTest.java
@@ -0,0 +1,23 @@
+package PruebasJUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import controller.Sesion;
+
+public class SesionTest {
+	
+	private SesionTest sesion = new SesionTest();
+
+	@Test
+	public void InicioSesion() {
+		Assert.assertEquals("jose123", true);
+		
+		
+		
+	}
+	
+
+}
