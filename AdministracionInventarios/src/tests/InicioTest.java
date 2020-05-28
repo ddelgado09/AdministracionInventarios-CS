f7e45b8fdd9101a48bdb70dcb22db6bd4bb056fb
Diego Delgado <ddelgado09@uan.edu.co>
2020-05-28 17:47:01 -0500

    test del menu inicio

---
+++ AdministracionInventarios/src/tests/InicioTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InicioTest {
+
+	
+		Inicio valor;
+
+		@Test
+		public void test() {
+			System.out.println("Inicio sesionr");
+			Inicio valor = new Inicio();
+			boolean dato = valor.InicioSesion(true, false);
+			Assert.assertNotEquals(dato, true);
+			
+			
+		}
+	
+
+}
